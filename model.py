e052a071736b4f50c9a4c910d1c396ea86d36687
Manca Prosek <manca.prosek@gmail.com>
2021-09-21 13:50:04 +0200

    Spremenjene nekatere metode v razredih in dodano zapisovanje v slovarje in iz slovarjev.

--- model.py
+++ model.py
@@ -1,33 +1,67 @@
+from tekstovni_vmesnik import dodaj_element
+
+import json
+
 class Model:
     def __init__(self):
         self.knjiznica = []
-        self.listek = {}
+        self.listek = []
 
 
-    def napisi_recept(self, ime, velikost, sestavine, postopek):
+    def napisi_recept(self, ime, velikost,  sestavine, postopek):
         recept = Recept(ime, velikost, sestavine, postopek)
         self.knjiznica.append(recept)
     
     def izbrisi_recept(self, indeks):
         self.knjiznica.pop(indeks)
 
-    def dodaj_recept(self, ime, kolicina, enota):
-        recept = self.knjiznica()
-
     def dodaj_element(self, ime, kolicina, enota):
         element = Element(self, ime, kolicina, enota)
-        if ime not in self.listek.keys():
-            self.listek(ime) = kolicina
-        else:
-            kolicina += self.listek(ime)
-            self.listek(ime) = kolicina
+        self.listek.append(element)
     
-    def izbrisi_element(self, ime):
-        self.listek.pop(ime)
+    def izbrisi_element(self, indeks):
+        self.listek.pop(indeks)
 
     def izbrisi_listek(self):
         self.listek.clear()
 
+    
+
+    def recepte_v_slovar(self):
+        return {
+            'ime': self.knjiznica,
+            'recepti': [recept.v_slovar() for recept in self.knjiznica]
+        }
+    
+    def elemente_v_slovar(self):
+        return {
+            'ime': self.listek ,
+            'elementi': [element.v_slovar() for element in self.listek]
+        }
+    
+
+    @staticmethod
+    def recepte_iz_slovarja(slovar_receptov):
+        knjiznica = Model(slovar_receptov['ime'])
+        knjiznica.recept = [Recept.iz_slovarja(knjiznica, recept) for recept in slovar_receptov['recepti']]
+        return knjiznica
+    
+    @staticmethod
+    def elemente_iz_slovarja(slovar_elementov):
+        listek = Model(slovar_elementov['ime'])
+        listek.element = [Element.iz_slovarja(listek, element) for element in slovar_elementov['elementi']]
+
+
+
+    def shrani_recepte(self, ime_datoteke):
+        with open(ime_datoteke, 'w') as dat:
+            slovar_receptov = self.recepte_v_slovar
+            json.dump(slovar_receptov, dat)
+    
+    def shrani_elemente(self, ime_datoteke):
+        with open(ime_datoteke, 'w') as dat:
+            slovar_elementov = self.elemente_v_slovar
+            json.dump(slovar_elementov, dat)
 
 
 
@@ -41,6 +75,28 @@ class Recept:
     def dodaj_sestavino(self, ime, kolicina, enota):
         sestavina = Sestavina(ime, kolicina, enota)
         self.sestavine.append(sestavina)
+    
+
+    def v_slovar(self):
+        return {
+            'ime': self.ime,
+            'velikost': self.velikost,
+            'sestavine': self.sestavine,
+            'postopek': self.postopek
+        }
+    
+    @staticmethod
+    def iz_slovarja(knjiznica, slovar_receptov):
+        return Recept(
+            knjiznica,
+            slovar_receptov['ime'],
+            slovar_receptov['velikost'],
+            slovar_receptov['sestavine'],
+            slovar_receptov['postopek'],
+        )
+
+    #manjka še urejanje recepta, spreminjanje velikosti in dodajanje na listek
+    #Kako pa s pisanjem velikosti in postopka?
 
 
 
@@ -50,6 +106,7 @@ class Sestavina:
         self.kolicina = kolicina
         self.enota = enota
 
+    #moram tukaj kaj posebaj še dati za spreminjanje? a sploh rabim te razrede?
 
 
 class Element:
@@ -57,3 +114,22 @@ class Element:
         self.ime = ime
         self.kolicina = kolicina
         self.enota = enota
+    
+
+    def v_slovar(self):
+        return {
+            'ime': self.ime,
+            'količina': self.kolicina,
+            'enota': self.enota
+        }
+    
+    @staticmethod
+    def iz_slovarja(listek, slovar_elementov):
+        return Element(
+            listek,
+            slovar_elementov['ime'],
+            slovar_elementov['količina'],
+            slovar_elementov['enota'],
+        )
+
+    #manjka urejanje elementa in združevanje istih. in tist slovar z enotami bi bil kul
