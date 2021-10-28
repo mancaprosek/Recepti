32ed866669ab721e3d8973911a6cbf47ac6fd5d6
Manca Prosek <manca.prosek@gmail.com>
2021-10-28 09:44:36 +0200

    Dodane enote pri sestavinah in komentar za delanje objektov

--- model2.py
+++ model2.py
@@ -5,8 +5,10 @@ class Model:
         self.knjiznica = []
 
     def dodaj_recept(self, ime, velikost,  sestavine, postopek):
-        recept = Recept(ime, velikost, sestavine, postopek)
+        recept = (ime, velikost, sestavine, postopek)
         self.knjiznica.append(recept)
+    
+    #tle zraven ni tega razreda recept
     
     def izbrisi_recept(self, indeks):
         self.knjiznica.pop(indeks)
@@ -23,13 +25,16 @@ class Recept:
         self.sestavine = {}
         self.postopek = postopek
     
-    def dodaj_sestavino(self, ime, kolicina):
-        sestavina = Sestavina(ime, kolicina)
-        self.sestavine[ime] = kolicina
+    def dodaj_sestavino(self, ime, kolicina, enota):
+        sestavina = (ime, kolicina, enota)
+        self.sestavine[ime] = (kolicina, enota)
+    
+    #ni razreda sestavina
 
 
 
 class Sestavina:
-    def __init__(self, ime, kolicina):
+    def __init__(self, ime, kolicina, enota):
         self.ime = ime
         self.kolicina = kolicina
+        self.enota = enota
