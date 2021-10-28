907afe511fd8d309e192a5c90f892cebed7214a9
Manca Prosek <manca.prosek@gmail.com>
2021-10-28 10:40:13 +0200

    Posodobljen spletni vmesnik na model2

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,15 +1,11 @@
 import bottle
 
-from model import Model, Recept
-
+from model2 import Model
 model = Model()
-
 
 @bottle.get('/')
 def osnovna_stran():
-    return bottle.template(
-        "osnovna_stran.html", recepti=model.knjiznica, listek=model.listek
-        )
+    return bottle.template("osnovna_stran.html", recepti=model.knjiznica)
 
 
 bottle.run(reloader=True, debug=True)
